4b1ec69a0103ee5bb88426485de83f2047332c56
qpf <qianpf0717@gmail.com>
2017-05-21 21:35:34 +0800

    reconstruct

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
   let $type = $(".timer .type");
   let breakTime = $breakLength.text();
   let sessionTime = $sessionLength.text();
-  let countSession = sessionTime * 60;
-  let countBreak = breakTime * 60;
+  // let countSession = sessionTime * 60;
+  // let countBreak = breakTime * 60;
   // $time.text(sessionTime);
   //add time control click listener
+  $time.text($sessionLength.text());
   $(".wrapper").click(function(event){
     let target=event.target;
     switch(target.id){
@@ -19,14 +20,16 @@ $(document).ready(function(){
         }else {
           breakTime--;
           $breakLength.text(breakTime);
-          countBreak = breakTime * 60;
+          // countBreak = breakTime * 60;
+          timer.initData();
           break;
         }
       case "break-plus":
         $("#break-minus").removeProp("disabled");
         breakTime++;
         $breakLength.text(breakTime);
-        countBreak = breakTime * 60;
+        // countBreak = breakTime * 60;
+        timer.initData();
         break;
       case "session-minus":
         if(sessionTime === 1){
@@ -36,7 +39,8 @@ $(document).ready(function(){
           sessionTime--;
           $sessionLength.text(sessionTime);
           $time.text(sessionTime);
-          countSession = sessionTime * 60;
+          // countSession = sessionTime * 60;
+          timer.initData();
           break;
         }
       case "session-plus":
@@ -44,106 +48,108 @@ $(document).ready(function(){
         sessionTime++;
         $sessionLength.text(sessionTime);
         $time.text(sessionTime);
-        countSession = sessionTime * 60;
+        // countSession = sessionTime * 60;
+        timer.initData();
         break;
     }
   });
-
-  let counting = false;//whether counting down
-  let type = "session";//counting type
-  $(".timer").click(function(event){
-    let $target = $(event.target);
-    if($target.is("p")){
-      //TODO start/stop the timer
-      var timer = {
-        start: function(){
-          counting = true;
-          $(".control button").attr("disabled","true");
-          interval_id = setInterval(function(){
-            countDown(type);
+  let id;//定时器Id
+  let OffState = function(timer) {
+    this.timer = timer;
+  };
+  OffState.prototype.panelWasClicked = function() {
+    console.log('start a timer');
+    counter(this.timer).start();
+    this.timer.setState(this.timer.onState);
+  };
+  let OnState = function(timer) {
+    this.timer = timer;
+  };
+  OnState.prototype.panelWasClicked = function() {
+    console.log('stop a timer');
+    counter(this.timer).stop();
+    this.timer.setState(this.timer.offState);
+  }
+  let Timer = function(btn){
+    this.offState = new OffState(this);
+    this.onState = new OnState(this);
+    this.btn = btn;
+  };
+  Timer.prototype.setState = function(nextState) {
+    this.currState = nextState;
+  };
+  Timer.prototype.getType = function() {
+    return this.type;
+  };
+  Timer.prototype.setType = function(type) {
+    this.type = type;
+  };
+  Timer.prototype.initData = function() {
+    this.sessionSeconds = $sessionLength.text() *60;
+    this.breakSeconds = $breakLength.text() * 60;
+    this.currCount = this.sessionSeconds;
+    this.totalCount = this.sessionSeconds;
+    this.type = 'SESSION';
+    this.currState = this.offState;
+  }
+  Timer.prototype.initCtrl = function() {
+    let self = this;
+    $(this.btn).on('click', 'p', function(e) {
+      self.currState.panelWasClicked();
+    });
+  }
+  let timer = new Timer($('.timer'));
+  timer.initData();
+  timer.initCtrl();
+  let counter = function(timer) {
+    //TODO then return a object
+    return {
+      start() {
+        console.log('counter start');
+        $(".control button").attr("disabled","true");
+        if(timer.currCount) {
+          id = setInterval(() => {
+            timer.currCount--;
+            let minute = Math.floor(timer.currCount/60);
+            let second = Math.floor(timer.currCount%60);
+            if(second < 10){
+              second = "0" + second;
+            }
+            $type.text(timer.getType());
+            $time.text(minute + ":" + second);
+            let fillHeight = (1-timer.currCount/timer.totalCount) * 100 + "%";
+            let fillColor = "#03bb7a";
+            $(".fill").css({
+              height : fillHeight,
+              background : fillColor
+            });
+            //倒计时为0时，切换另外一种模式
+            if(!timer.currCount) {
+              clearInterval(id);
+              changeType(timer);
+              this.start();
+            }
           },1000);
-        },
-        stop: function(){
-          counting = false;
-          $(".control button").removeAttr("disabled");
-          clearInterval(interval_id);
         }
-      };
-      if(!counting){
-        timer.start();
-      }else {
-        timer.stop();
+      },
+      stop() {
+        $(".control button").removeAttr("disabled");
+        clearInterval(id);
       }
     }
-  });
-  function countDown(t){
-    //TODO count down,when time === 0,toggle
-    if(t === "session"){
-      //TODO count down session time
-      if(countSession > 0){
-        countSession--;
-        let minute = Math.floor(countSession/60);
-        let second = Math.floor(countSession%60);
-        if(second < 10){
-          second = "0" + second;
-        }
-        $type.text("SESSION");
-        $time.text(minute + ":" + second);
-        let fillHeight = (1-countSession/sessionTime/60) * 100 + "%";
-        let fillColor = "#03bb7a";
-        $(".fill").css({
-          height : fillHeight,
-          background : fillColor
-        });
-      }else {
-        //TODO reset session time and setup break time
-        countSession = sessionTime * 60;
-        type = "break";
-        $(".fill").css({
-          height: 0,
-          background: "transparent"
-        });
-        let minute = Math.floor(countBreak/60);
-        let second = Math.floor(countBreak%60);
-        if(second < 10){
-          second = "0" + second;
-        }
-        $type.text("BREAK");
-        $time.text(minute + ":" + second);
-      }
-    }else if (t === "break"){
-      //TODO count down break time
-      if(countBreak > 0){
-        countBreak--;
-        let minute = Math.floor(countBreak/60);
-        let second = Math.floor(countBreak%60);
-        if(second < 10){
-          second = "0" + second;
-        }
-        $type.text("BREAK");
-        $time.text(minute + ":" + second);
-        let fillHeight = (1-countBreak/breakTime/60) * 100 + "%";
-        let fillColor = "#ea4545";
-        $(".fill").css({
-          height : fillHeight,
-          background : fillColor
-        });
-      }else {
-        //TODO reset break time and setup session time
-        countBreak = breakTime * 60;
-        type = "session";
-        $(".fill").css({
-          height: 0,
-          background: "transparent"
-        });
-        let minute = Math.floor(countSession/60);
-        let second = Math.floor(countSession%60);
-        if(second < 10){
-          second = "0" + second;
-        }
-        $type.text("SESSION");
-        $time.text(minute + ":" + second);
-      }
+  };
+  function changeType(timer) {
+    function isSession(timer) {
+      return timer.getType() === 'SESSION';
+    }
+    if(isSession(timer)) {
+      timer.currCount = timer.breakSeconds;
+      timer.totalCount = timer.breakSeconds;
+      timer.setType('BREAK');
+    }else {
+      timer.currCount = timer.sessionSeconds;
+      timer.totalCount = timer.sessionSeconds;
+      timer.setType('SESSION');
     }
   }
 });
